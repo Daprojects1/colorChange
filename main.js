95c54507fa422d9938e8c7adf032ffc0621e66ad
Daprojects1 <davidrtm45@gmail.com>
2023-06-19 17:08:05 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+import './style.css'
+import { handleClick } from './event'
+
+
+document.querySelector('#app').innerHTML = `
+  <div class=body>
+  <h1>Color Flipper</h1>
+  <div class=bgchange>
+    <div>Background Color: <span class=color> #FFFFFF</span></div>
+    <button class=btn>Press me</button>
+  </div>
+  </div>
+`
+
+handleClick(document.querySelector('.btn'))
